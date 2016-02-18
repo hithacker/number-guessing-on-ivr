f7289e0ed5713085d5fc012e233c5cb027d341df
Hiren Thacker <hithacker@Hirens-MacBook-Pro.local>
2016-02-18 16:02:58 +0530

    Collect dtmf

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
+                 [org.clojure/data.xml "0.0.8"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,12 +4,21 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.data.xml :as xml]))
+
+(def guessed-number (atom nil))
 
 (defn splash []
+  (reset! guessed-number (rand-int 100))
   {:status 200
    :headers {"Content-Type" "application/xml"}
-   :body "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response> \n    <playtext>Hello World</playtext>\n<hangup></hangup></Response>"})
+   :body (xml/emit-str
+           (xml/sexp-as-element
+             [:response
+              [:collectdtmf
+               [:playtext "Enter the number"]]
+              [:hangup]]))})
 
 (defroutes app
   (GET "/" []
@@ -20,6 +29,9 @@
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+(defn create-xml [vec]
+  (xml/emit-str (xml/sexp-as-element vec)))
 
 ;; For interactive development:
 ;; (.stop server)
